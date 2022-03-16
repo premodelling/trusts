3dc8858fcefc9bc90f4e5c8cc75801192e73016f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-03-16 22:05:09 +0000

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -11,7 +11,7 @@ source(file = 'R/EvaluationHistories.R')
 source(file = 'R/EvaluationEndpointsData.R')
 source(file = 'R/EvaluationEndpoints.R')
 
-melted <- EvaluationHistoriesData()
+melted <- EvaluationHistoriesData(methods = c('CNN', 'LSTM'))
 ValidationHistoryMAE(melted = melted)
 ValidationHistoryLoss(melted = melted)
 
