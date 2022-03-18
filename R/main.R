f5671e990deaa991fce5714c4f5431267b9b5f3d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2022-03-18 20:02:24 +0000

    Update R/main.R

--- R/main.R
+++ R/main.R
@@ -11,12 +11,31 @@ source(file = 'R/EvaluationHistories.R')
 source(file = 'R/EvaluationEndpointsData.R')
 source(file = 'R/EvaluationEndpoints.R')
 
-melted <- EvaluationHistoriesData(methods = c('CNN', 'LSTM'))
-ValidationHistoryMAE(melted = melted)
-ValidationHistoryLoss(melted = melted)
+# the function EvaluationHistoriesData(.) requires the method/directory names, of interest, in your local
+# infections project/package
+#     infections/warehouse/modelling/evaluations/histories
+#
+histories <- EvaluationHistoriesData(methods = c('CNN', 'LSTM'))
+ValidationHistoryMAE(histories = histories)
+ValidationHistoryLoss(histories = histories)
 
 endpoints <- EvaluationEndpointsData()
 EndpointsMAE(endpoints = endpoints)
 EndpointsLoss(endpoints = endpoints)
+
+
+# Prepare directory
+pathstr <- file.path(getwd(), 'warehouse', 'modelling', 'outcomes')
+if (dir.exists(paths = pathstr)) {
+  base::unlink(pathstr, recursive = TRUE)
+}
+dir.create(path = pathstr, showWarnings = TRUE, recursive = TRUE)
+
+graphing <- function (data, filename) {
+  data.table::fwrite(x = setDT(data), file = file.path(pathstr, paste0(filename, '.csv')))
+}
+mapply(FUN = graphing, data = list(histories, endpoints), filename = list('histories', 'endpoints'))
+
+
 
 
